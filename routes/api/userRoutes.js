3d9a5468712280fd07a73c72d8a54e586ffd9524
Chelsea Wagner <1chelsea.fischer1@gmail.com>
2023-10-17 21:28:15 -0700

    finalized routes with friends and reactions in the users and thoughts routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,13 +4,18 @@ const {
     getUserById,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    addFriend,
+    deleteFriend
 } = require('../../controllers/userController');
 
-// /api/users
+// /api/users Get all users and create a user
 router.route('/').get(getAllUsers).post(createUser);
 
-// /api/users/:userId
+// /api/users/:userId get one user, update and delete a user
 router.route('/:userId').get(getUserById).put(updateUser).delete(deleteUser);
+
+//api/users/:userId/friends/:friendId add and delete a friend
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
